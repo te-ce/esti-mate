05e66824fc908f99f33261a72895efb4d686cffe
te-ce <9517685+te-ce@users.noreply.github.com>
2025-03-24 16:59:01 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# ESTI-MATE
+
+Your last mate to estimate your tickets
+
+## Motivation
+
+Goal of this project is to build a tool that can help to estimate the tickets. At best this is implemented without the need of an actual Server (WebRTC?). Motivation is to learn and understand the basics of web.
+
+## Roadmap
+
+First implementation will be with a server, understand how HTML and forms work. Then try to implement to strip away the server.
---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
